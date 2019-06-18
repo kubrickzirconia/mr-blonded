3bcee7dbe196230e4bb597877907d513b0bc7a83
Lily <lilyblake@outlook.com>
2019-06-18 10:17:21 +0100

    Create tumblr.js

---
+++ code/tumblr.js
@@ -0,0 +1,151 @@
+    const dataSample = [
+      {
+        date: 'September 2010',
+        value: 20,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2010/9"
+        },
+      {
+        date: 'October 2010',
+        value: 15,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2010/10"
+        },
+      {
+        date: 'November 2010',
+        value: 11,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2010/11"
+        },
+      {
+        date: 'December 2010',
+        value: 6,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2010/12"
+        },   
+      {
+        date: 'January 2011',
+        value: 11,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2011/1"
+        },
+      {
+        date: 'February 2011',
+        value: 8,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2011/2"
+        },
+      {
+        date: 'March 2011',
+        value: 19,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2011/3"
+        },
+      {
+        date: 'April 2011',
+        value: 25,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2011/4"
+        },
+      {
+        date: 'May 2011',
+        value: 13,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2011/5"
+        },
+      {
+        date: 'June 2011',
+        value: 48,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2011/6"
+        },
+      {
+        date: 'July 2011',
+        value: 46,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2011/7"
+        },
+      {
+        date: 'August 2011',
+        value: 48,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/2011/8"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },      {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },      {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+              {
+        date: '',
+        value: ,
+        colour: '#000000',
+        link: "https://frankocean.tumblr.com/archive/"
+        },
+ 
+    ];
