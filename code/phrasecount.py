99a4c5265598f9e37e48b1e53b60160ba20f1189
Lily <lilyblake@outlook.com>
2019-08-02 16:02:10 +0100

    Update phrasecount.py

--- code/phrasecount.py
+++ code/phrasecount.py
@@ -1,5 +1,6 @@
 text = str(input("Copy and paste text here:"))
 key_phrase = str(input("Key phrase to search for:"))
+text = text.lower()
 counter = text.count(key_phrase)
 print("Number of occurences: ")
 print(counter)
